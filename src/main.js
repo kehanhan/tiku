f3b0a5770fbad482ef8795f56949bb2d45827e48
kehan <ke_han@icloud.com>
2022-09-24 19:56:09 +0800

    安装varlet-ui

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style.js'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(Varlet).mount('#app')
